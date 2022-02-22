a34e3fa2fe42aa14884a71e128c1b48c549b0d65
Daiva Rajackiene <daiva.rajackiene@gmail.com>
2022-02-22 15:34:48 +0200

    we get all cars

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,15 +1,1 @@
-const mysql = require('mysql2/promise');
-const dbConfig = require('../configDb');
 
-async function singleDbExecute(sql, id) {
-  try {
-    const connection = await mysql.createConnection(dbConfig);
-
-    const [data] = await connection.execute(sql, [id]);
-    await connection.close();
-    return data;
-  } catch (error) {
-    console.log('klaida', error);
-    return false;
-  }
-}
